adc4e9f3cda2b6c225c9bdc8759e6214beb1760f
rhys <rbrooker105@gmail.com>
2016-08-16 21:52:42 +1000

    Added matching code methods and tested succesfully

--- lib/master_mind.rb
+++ lib/master_mind.rb
@@ -4,5 +4,5 @@ module MasterMind
   # Your code goes here...
 end
 require_relative "./master_mind/cell.rb"
-#require_relative "./master_mind/game.rb"
+require_relative "./master_mind/game.rb"
 require_relative "./master_mind/board.rb"
--- lib/master_mind/game.rb
+++ lib/master_mind/game.rb
@@ -1,8 +1,28 @@
-Module MasterMind
+module MasterMind
   class Game
   	attr_accessor :grid
   	def generate_key
   	  Cell.new([rand(1..6),rand(1..6),rand(1..6),rand(1..6)])
+  	end
+  	def check_matches(code, guess)
+  	  matches = 0
+  	  guess.each_with_index do |number, index|
+  	  	if number == code[index]
+  	  	  matches += 1
+  	  	end 
+  	  end
+  	  matches
+  	end
+  	def check_unordered_matches(code,guess)
+  	  exact_matches = check_matches(code, guess)
+  	  matches = 0
+  	  guess.each do |number|
+  	  	if code.include?(number)
+  	  	  code.delete_at(code.index(number)) #Delete's the match we just made
+  	  	  matches += 1
+  	  	end
+  	  end
+  	  matches = matches - exact_matches
   	end
   end
 end
