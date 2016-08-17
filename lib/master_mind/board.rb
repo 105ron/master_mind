d73220f1c8d5425146eaf18633d1db9a430c97c0
rhys <rbrooker105@gmail.com>
2016-08-17 19:08:09 +1000

    Change game code methods into the Board class

--- lib/master_mind/board.rb
+++ lib/master_mind/board.rb
@@ -1,5 +1,31 @@
 module MasterMind
+
   class Board
+
+  	def check_matches(code, guess)
+  	  matches = 0
+  	  guess.each_with_index do |number, index|
+  	  	if number == code[index]
+  	  	  matches += 1
+  	  	end 
+  	  end
+  	  matches
+  	end
+
+
+  	def check_unordered_matches(code,guess)
+  	  exact_matches = check_matches(code, guess)
+  	  matches = 0
+  	  guess.each do |number|
+  	  	if code.include?(number)
+  	  	  code.delete_at(code.index(number)) #Delete's the match we just made
+  	  	  matches += 1
+  	  	end
+  	  end
+  	  matches = matches - exact_matches
+  	end
+
+
   	def number_to_colors(number)
   	  case number
   	  when 1 then "blue"
@@ -7,8 +33,22 @@ module MasterMind
   	  when 3 then "orange"
   	  when 4 then "green"
   	  when 5 then "yellow"
-  	  else        "pink"
+  	  else "pink"
   	  end
   	end
+
+
+  	def color_to_numbers(color)
+  	  case number
+  	  when "blue" then 1
+  	  when "purple" then 2
+  	  when "orange" then 3
+  	  when "green" then 4
+  	  when "yellow" then 5
+  	  else 6
+  	  end
+  	end
+
   end
+
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,45 +2,6 @@ require "spec_helper"
  
 module MasterMind
   describe Game do
-    context "#check_matches" do  
-      it "It returns the correct amount of matches" do
-    	  game = Game.new
-    	  expect(game.check_matches([2,4,5,3],[2,4,2,3])).to eq 3
-      end
-      it "It ignores a match of elements in the wrong order" do
-        game = Game.new
-        expect(game.check_matches([7,6,2,1],[1,2,7,6])).to eq 0
-      end
-      it "It correctly handles repeated code characters" do
-        game = Game.new
-        expect(game.check_matches([1,1,1,3],[1,1,1,4])).to eq 3
-      end
-      it "It doesn't match repeated elements out of order" do
-        game = Game.new
-        expect(game.check_matches([1,1,1,1],[0,1,2,1])).to eq 2
-      end
-      it "It correctly matches 4 of the same number" do
-        game = Game.new
-        expect(game.check_matches([1,1,1,1],[1,1,1,1])).to eq 4
-      end
-    end
-    context "#check_unordered_matches" do
-      it "It returns matches even if they are out of order" do
-        game = Game.new
-        expect(game.check_unordered_matches([2,5,3,4],[5,4,2,3])).to eq 4
-      end
-      it "If code has repeated numbers but our guess doesn't, the repeats are ignored" do
-        game = Game.new
-        expect(game.check_unordered_matches([1,6,1,1],[2,1,7,9])).to eq 1
-      end
-      it "If guess has repeated numbers but our code doesn't, the repeats are ignored" do
-        game = Game.new
-        expect(game.check_unordered_matches([3,7,2,1],[1,1,1,4])).to eq 1
-      end
-      it "It doesn't count matches in the correct position" do
-        game = Game.new
-        expect(game.check_unordered_matches([1,1,3,1],[1,1,1,3])).to eq 2
-      end
-    end
+    
   end
 end
